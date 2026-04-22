6ff5ddd5fbb88b01b99c552cf5708f6302ea7448
Neelanjana-coder <neelu.me190@gmail.com>
2026-04-22 14:41:41 +0530

    initial commit

---
+++ github/calculator.py
@@ -0,0 +1,15 @@
+a=float(input("Enter a number:"))
+b=float(input("Enter a number:"))
+op=input("Enter operator:")
+if op=='+':
+    print("result=",a+b)
+elif op=='-':
+    print("result=",a-b)
+elif op=='/':
+    print("result=",a/b)
+elif op=='*':
+    print("result=",a*b)
+else:
+    print("Invalid")
+
+
