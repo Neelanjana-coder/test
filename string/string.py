4cddd6174f87a97a0761b9f3d41a01f3a5353a94
Neelanjana-coder <neelu.me190@gmail.com>
2026-04-23 12:21:30 +0530

    initial commit

--- string/string.py
+++ string/string.py
@@ -1,5 +1,5 @@
-test=input("Enter a string:")
-if test == test[::-1]:
+test = input("Enter a string:")
+if test==test[::-1]:
     print("Palindrome")
 else:
-    print(" Not a palinrome")
+    print("Not a palindrome")
