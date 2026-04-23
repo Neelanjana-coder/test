c9ac6714c2102646e86128cd974057d98443355d
Neelanjana-coder <neelu.me190@gmail.com>
2026-04-23 13:33:21 +0530

    initial commit

---
+++ if.py
@@ -0,0 +1,6 @@
+age=20
+if age>=18:
+    print("is eligible for license")
+else:
+    print("is not eligible for license")
+    
---
+++ personaldata.py
@@ -0,0 +1,8 @@
+name="Neelanjana"
+age=20
+height=156
+result="pass"
+print(name)
+print(age)
+print(height)
+print(result)
